cb950058f7bea6046bdc00dc225ec23b82087584
hamzapehlivan <hamza.pehlivan@bilkent.edu.tr>
2022-03-15 20:49:53 +0300

    Bug Fix In Main

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from cvlib.face_detector import face_detector
 import cv2
 
 image = cv2.imread("data/69996.png", cv2.IMREAD_COLOR)
-faces = face_detector(image)
+faces = face_detector.detect_faces(image)
 
 ## Compare Target Face with Every Found Faces. Use SIFT or something else
