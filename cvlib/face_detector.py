66b2c94ab6d5d08b280b951b648b8ee062ef412c
hamzapehlivan <hamza.pehlivan@bilkent.edu.tr>
2022-04-06 12:47:50 +0300

    Match features between faces and masked image

--- cvlib/face_detector.py
+++ cvlib/face_detector.py
@@ -8,6 +8,13 @@ def detect_faces(in_image):
     faces = haar.detectMultiScale(image, minNeighbors=9, minSize=(100,100))
     return faces
 
+def draw_faces(image, faces):
+    image = image.copy()
+    for face in faces:
+        x,y,w,h = face
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 3)
+    cv2.imwrite("results/detected_faces.png", image)
+
 #Test the function
 if __name__ == '__main__':
     image = cv2.imread("data/69996.png", cv2.IMREAD_COLOR)
--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-from cvlib.face_detector import face_detector
+from cvlib import face_detector, mask_generator
 import cv2
+import copy
 
-image = cv2.imread("data/69996.png", cv2.IMREAD_COLOR)
-faces = face_detector.detect_faces(image)
+image_target = cv2.imread("data/target.png", cv2.IMREAD_COLOR)
+mask = mask_generator.RandomMask(image_target.shape[0])
+masked_target = image_target * mask
+cv2.imwrite("results/masked_image.png", masked_target)
+image_ref = cv2.imread("data/reference.png", cv2.IMREAD_COLOR)
 
-## Compare Target Face with Every Found Faces. Use SIFT or something else
+## Find Faces
+faces = face_detector.detect_faces(image_ref)
+face_detector.draw_faces(image_ref, faces)
+
+
+## Compare Target Face with Every Found Faces with SIFT
+sift = cv2.SIFT_create()
+matcher = cv2.BFMatcher.create(normType = cv2.NORM_L2, crossCheck=False)
+
+kp1, des1 = sift.detectAndCompute(masked_target, mask)
+out = cv2.drawKeypoints(masked_target, kp1, 0, (0, 255, 0),flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+cv2.imwrite("results/target_kps.png", out)
+
+best_score= 0
+detected_matches = []
+detected_face = -1
+detected_kps = -1
+for face in faces:
+    x,y,w,h = face
+    face_img = image_ref[y:y+h, x:x+w]
+    kp2, des2 = sift.detectAndCompute(face_img, None)
+    matches = matcher.knnMatch(des1,des2,k=2)
+
+    #Knn ratio test
+    best_mathces = []
+    for nearest, second_nearest in matches:
+        if (nearest.distance / second_nearest.distance < 0.7):
+            best_mathces.append([nearest])
+    if ( len(best_mathces) > best_score):
+        best_score = len (best_mathces)
+        detected_matches = best_mathces
+        detected_face = face_img
+        detected_kp = kp2
+    
+out = cv2.drawMatchesKnn(masked_target,kp1,detected_face,detected_kp,detected_matches,None)
+cv2.imwrite("results/matches.png", out)
+
